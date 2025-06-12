cfc71e954cf5be37f43bcce1f5c3f61bba4a934c
Davi Mariano <anlanzinka@gmail.com>
2025-06-11 21:06:47 -0300

    Adição dos arquivos mais importantes do site
    Reformulei completamente o HTML, fiz o CSS e o JS e por fim adicionei as imagens.

---
+++ js/script.js
@@ -0,0 +1,6 @@
+    function toggleSidebar() {
+      const sidebar = document.getElementById('sidebar');
+      const overlay = document.querySelector('.overlay');
+      sidebar.classList.toggle('active');
+      overlay.classList.toggle('active');
+    }
