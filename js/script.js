405ece4d22cfa6af5a0e96d640af5591cd316b93
Davi Mariano <anlanzinka@gmail.com>
2025-08-27 07:02:20 -0300

    Delete js directory

--- js/script.js
+++
@@ -1,6 +0,0 @@
-    function toggleSidebar() {
-      const sidebar = document.getElementById('sidebar');
-      const overlay = document.querySelector('.overlay');
-      sidebar.classList.toggle('active');
-      overlay.classList.toggle('active');
-    }
